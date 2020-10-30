d73b3e5c23d25b983188a3e3ad99ad963626370e
paw2110 <pawmadsen.2002@gmail.com>
2020-10-30 11:02:36 +0100

    logiske operatorer

---
+++ logiske operatorer/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    line(width/2,0,width/2,height)
+    line(0,width/2,width,height/2)
+    square(mouseX,mouseY,75)
+    if(mouseX<=width/2&&mouseY<=height/2||(mouseX>=width/2&&mouseY>=height/2))
+      fill(255,255,255)
+    if(mouseX<=width/2&&mouseY>=height/2||(mouseX>=width/2&&mouseY<=height/2))
+      fill(255,0,0)    
+  }
