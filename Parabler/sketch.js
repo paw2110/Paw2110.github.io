658ba7cb73f130b8f61ec6aff83d4b7b14c135fb
paw2110 <pawmadsen.2002@gmail.com>
2020-11-19 12:51:26 +0100

    commit

---
+++ Parabler/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    fill(0,0,0) 
+  }
+  
+  function draw() {
+    background(220);
+    circle(20,20,10)
+    circle(580,20,10)
+    circle(width/2,mouseY,10)
+    line(20,20,300,mouseY)
+    line(580,20,300,mouseY)  
+   
+  }
