ec33da0e235c7be773a991b80c713c73705ba004
paw2110 <pawmadsen.2002@gmail.com>
2020-12-03 12:33:51 +0100

    Mangebouncebolde

---
+++ hoppe bolde.2/sketch.js
@@ -0,0 +1,51 @@
+let balls = new Array(50);
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    for (let i = 0; i < balls.length; i++) {
+        balls[i] = new RandomBall(width / 2, height / 2);
+    }
+}
+
+function draw() {
+    background(220);
+
+    for (let i = 0; i < balls.length; i++) {
+        const b = balls[i];
+        b.render();
+        b.update();
+    }
+}
+
+class RandomBall {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.d = random(50, 150)
+
+        const sMax = 20
+        this.speedX = random(-sMax, sMax)
+        this.speedY = random(-sMax, sMax)
+        this.color = color(random(0, 255),random(0, 255),random(0, 255))
+    }
+
+    update() {
+        this.x += this.speedX
+        this.y += this.speedY
+
+        if (this.x + this.d > width || this.x - this.d < 0) {
+            this.speedX = -this.speedX
+        }
+
+        if (this.y + this.d > height || this.y - this.d < 0) {
+            this.speedY = -this.speedY
+        }
+    }
+
+    render() {
+        circle(this.x, this.y, this.d);
+        fill(this.color)
+    }
+}
+
+
